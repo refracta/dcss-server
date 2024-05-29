791a07a5cc9719b7c37af0be2dc35023385689f0
refracta <58779799+refracta@users.noreply.github.com>
2024-05-29 18:14:25 +0900

    Fix compile error (Version 0.24) (#26)
    * Minor fix
    
    * Minor fix

--- crawl-build/update-crawl-stable-build.sh
+++ crawl-build/update-crawl-stable-build.sh
@@ -49,15 +49,11 @@ CXX="ccache g++"
 if (( VERSION_INT <= 24 )) && [[ -f $CRAWL_REPOSITORY_DIR/crawl-ref/source/util/species-gen.py ]]; then
   echo "Patching collections.MutableMapping to collections.abc.MutableMapping in species-gen.py..."
   sed -i 's/collections.MutableMapping/collections.abc.MutableMapping/g' $CRAWL_REPOSITORY_DIR/crawl-ref/source/util/species-gen.py
+  echo "Patching yaml.load(open(f_path)) to yaml.load(open(f_path), Loader=yaml.FullLoader) in species-gen.py..."
+  sed -i 's/yaml.load(open(f_path))/yaml.load(open(f_path), Loader=yaml.FullLoader)/g' $CRAWL_REPOSITORY_DIR/crawl-ref/source/util/species-gen.py
 fi
 
 if (( VERSION_INT <= 24 )); then
-  echo "Setting compiler to gcc-7 and g++-7..."
-  CC="ccache gcc-7"
-  CXX="ccache g++-7"
-fi
-
-if (( VERSION_INT <= 20 )); then
   echo "Setting compiler to gcc-6 and g++-6..."
   CC="ccache gcc-6"
   CXX="ccache g++-6"
