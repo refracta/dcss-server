1b61844da11335f83a04622be51696fb52e087d2
refracta <refracta@abstr.net>
2024-06-12 13:04:36 +0900

    Minor fix

--- server/scripts/static/entrypoint.sh
+++ server/scripts/static/entrypoint.sh
@@ -5,6 +5,8 @@ if [ ! -d "$DGL_CONF_HOME" ] || [ -z "$(ls -A "$DGL_CONF_HOME")" ]; then
     cp -r /usr/src/dgamelaunch-config/* "$DGL_CONF_HOME"
 fi
 
+ls -A $DGL_CONF_HOME
+
 entrypoint.sh
 echo "$PATH"
 tail -f /dev/null
