fc87be617c558a36548338acc5d8a41ad9c475e1
refracta <58779799+refracta@users.noreply.github.com>
2024-06-13 15:43:10 +0900

    Minor fix

--- server/scripts/deploy/one-line-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -o docker-compose.yml https://refracta.github.io/dcss-server/server/docker-compose.yml && \
-curl -o docker-compose.override.yml https://refracta.github.io/dcss-server/server/docker-compose.ports.yml && \
-docker compose down && docker rmi refracta/dcss-server || true && \
-docker compose run --rm -T -e CMD='$SCRIPTS/utils/release.sh download -o -p /data -n game-data' dcss-server && \
-docker compose up -d && docker compose logs -f
