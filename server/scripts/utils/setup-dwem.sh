99c321a612a2675884390b53e232ecb019b76d6d
refracta <refracta@abstr.net>
2025-10-23 19:59:58 +0900

    Minor fix

--- server/scripts/utils/setup-dwem.sh
+++ server/scripts/utils/setup-dwem.sh
@@ -2,4 +2,4 @@
 
 source "$DGL_CONF_HOME/dgl-manage.conf"
 TIMESTAMP=$(date +%s%3N)
-sed -i 's|<script src="/static/scripts/contrib/require\.js[^"]*" data-main="/static/scripts/app"></script>|<script src="https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module/loader/dwem-base-loader.js?t='"$TIMESTAMP"'"></script>|g' "$WEBDIR/templates/client.html"
+sed -i 's|<script src="{{ static_url("scripts/contrib/require.js") }}" data-main="/static/scripts/app"></script>|<script src="https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module/loader/dwem-base-loader.js?t='"$TIMESTAMP"'"></script>|g' "$WEBDIR/templates/client.html"
