5c8944369f333da31432805f8d306503d04a396f
refracta <refracta@users.noreply.github.com>
2026-05-11 09:35:40 +0900

    Fix savegame-transfer-all glob expansion

--- bin/savegame-transfer-all.sh
+++ bin/savegame-transfer-all.sh
@@ -6,18 +6,28 @@
 source "$DGL_CONF_HOME/crawl-git.conf"
 
 LATEST_GAME_HASH="$(latest-game-hash)"
-PREFIX="$DGL_CHROOT/$CRAWL_GIT_DIR"
+CRAWL_GIT_DIR="${CRAWL_GIT_DIR:-$CHROOT_CRAWL_BASEDIR}"
+BINARY_BASE_NAME="${BINARY_BASE_NAME:-$GAME}"
+PREFIX="$DGL_CHROOT$CRAWL_GIT_DIR"
 
-SAVEBASE="${PREFIX}/${BINARY_BASE_NAME}-*/saves"
-ALL_CHARS="$(ls -1rt "$SAVEBASE"/*-"${DGL_UID}".sav \
-                     "$SAVEBASE"/*-"${DGL_UID}".chr \
-                     "$SAVEBASE"/*.cs \
-                     "$SAVEBASE"/sprint/*-"${DGL_UID}".chr \
-                     "$SAVEBASE"/sprint/*.cs \
-                     "$SAVEBASE"/zotdef/*.cs 2>/dev/null | \
-             grep -v "$LATEST_GAME_HASH" | \
-             sed "s|${PREFIX}/${BINARY_BASE_NAME}-.*/saves/\(.*\)\..*|\1|;s|-${DGL_UID}||" | \
-             sort -f)"
+shopt -s nullglob
+SAVEFILES=(
+    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/*-"${DGL_UID}".sav
+    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/*-"${DGL_UID}".chr
+    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/*.cs
+    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/sprint/*-"${DGL_UID}".chr
+    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/sprint/*.cs
+    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/zotdef/*.cs
+)
+
+if ((${#SAVEFILES[@]})); then
+    ALL_CHARS="$(ls -1rt "${SAVEFILES[@]}" 2>/dev/null | \
+                 grep -v "/${BINARY_BASE_NAME}-${LATEST_GAME_HASH}/" | \
+                 sed "s|${PREFIX}/${BINARY_BASE_NAME}-.*/saves/\(.*\)\..*|\1|;s|-${DGL_UID}||" | \
+                 sort -f)"
+else
+    ALL_CHARS=""
+fi
 
 echo "Trying to transfer these chars to a newer version:"
 echo "${ALL_CHARS}"
