a3b7ad2bf4ee8a70f3c985fbd0ce38a79d06031e
refracta <refracta@users.noreply.github.com>
2026-05-11 09:46:08 +0900

    Use GAME directly for savegame-transfer-all

--- bin/savegame-transfer-all.sh
+++ bin/savegame-transfer-all.sh
@@ -7,23 +7,22 @@ source "$DGL_CONF_HOME/crawl-git.conf"
 
 LATEST_GAME_HASH="$(latest-game-hash)"
 CRAWL_GIT_DIR="${CRAWL_GIT_DIR:-$CHROOT_CRAWL_BASEDIR}"
-BINARY_BASE_NAME="${BINARY_BASE_NAME:-$GAME}"
 PREFIX="$DGL_CHROOT$CRAWL_GIT_DIR"
 
 shopt -s nullglob
 SAVEFILES=(
-    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/*-"${DGL_UID}".sav
-    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/*-"${DGL_UID}".chr
-    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/*.cs
-    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/sprint/*-"${DGL_UID}".chr
-    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/sprint/*.cs
-    "$PREFIX"/"$BINARY_BASE_NAME"-*/saves/zotdef/*.cs
+    "$PREFIX"/"$GAME"-*/saves/*-"${DGL_UID}".sav
+    "$PREFIX"/"$GAME"-*/saves/*-"${DGL_UID}".chr
+    "$PREFIX"/"$GAME"-*/saves/*.cs
+    "$PREFIX"/"$GAME"-*/saves/sprint/*-"${DGL_UID}".chr
+    "$PREFIX"/"$GAME"-*/saves/sprint/*.cs
+    "$PREFIX"/"$GAME"-*/saves/zotdef/*.cs
 )
 
 if ((${#SAVEFILES[@]})); then
     ALL_CHARS="$(ls -1rt "${SAVEFILES[@]}" 2>/dev/null | \
-                 grep -v "/${BINARY_BASE_NAME}-${LATEST_GAME_HASH}/" | \
-                 sed "s|${PREFIX}/${BINARY_BASE_NAME}-.*/saves/\(.*\)\..*|\1|;s|-${DGL_UID}||" | \
+                 grep -v "/${GAME}-${LATEST_GAME_HASH}/" | \
+                 sed "s|${PREFIX}/${GAME}-.*/saves/\(.*\)\..*|\1|;s|-${DGL_UID}||" | \
                  sort -f)"
 else
     ALL_CHARS=""
