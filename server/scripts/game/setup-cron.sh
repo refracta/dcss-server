32844134dc07c41ae5df4c41f9854bdc409c7073
refracta <refracta@abstr.net>
2026-08-03 22:09:32 +0900

    Stagger scheduled Crawl builds

--- server/scripts/game/setup-cron.sh
+++ server/scripts/game/setup-cron.sh
@@ -2,25 +2,28 @@
 
 # Define the commands and their schedules
 command1="env - \$(cat /proc/1/environ | tr '\\0' '\\n') /home/crawl-dev/dgamelaunch-config/bin/dgl update-trunk >> /home/crawl-dev/logs/trunk.log 2>&1"
-schedule1="*/15 * * * *"
+# Container cron runs in UTC. During fork update hours, let the fork take the
+# build lock at :00 and have trunk make a single attempt at :01.
+schedule1="*/15 0,6-23 * * *"
+schedule1_fork_hours="1 1-5 * * *"
 
 command2="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc stoatsoup stoatsoup/master >> /home/crawl-dev/logs/stoatsoup.log 2>&1"
-schedule2="0 6 * * *"
+schedule2="0 1 * * *"
 
 command3="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc bcadrencrawl bcadrencrawl/bCrawl >> /home/crawl-dev/logs/bcadrencrawl.log 2>&1"
-schedule3="0 7 * * *"
+schedule3="0 2 * * *"
 
 command4="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc bcrawl bcrawl/master >> /home/crawl-dev/logs/bcrawl.log 2>&1"
-schedule4="0 8 * * *"
+schedule4="0 3 * * *"
 
 command5="/home/crawl-dev/dgamelaunch-config/bin/dgl compress-ttyrecs"
 schedule5="15,30,45 * * * *"
 
 command6="/home/crawl-dev/dgamelaunch-config/bin/dgl update-stable 0.34 >> /home/crawl-dev/logs/0.34.log 2>&1"
-schedule6="0 9 * * *"
+schedule6="0 5 * * *"
 
 command7="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc dcst dcst/test >> /home/crawl-dev/logs/dcst.log 2>&1"
-schedule7="0 10 * * *"
+schedule7="0 4 * * *"
 
 
 # Check if a crontab file exists for the user, create one if not
@@ -30,6 +33,7 @@ fi
 
 # Add the commands and schedules to the crontab file
 { echo "$schedule1 $command1"; } >> "$HOME/crontab.txt"
+{ echo "$schedule1_fork_hours $command1"; } >> "$HOME/crontab.txt"
 { echo "$schedule2 $command2"; } >> "$HOME/crontab.txt"
 { echo "$schedule3 $command3"; } >> "$HOME/crontab.txt"
 { echo "$schedule4 $command4"; } >> "$HOME/crontab.txt"
